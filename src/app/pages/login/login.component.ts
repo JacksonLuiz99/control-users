6948f33ea08e68ef752d10f4af37e622ec0e9286
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2025-01-09 22:05:31 -0400

    TS para login (EStrutura, funções e variavél)

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
   userName: string;
+
+  constructor(private rota: Router) {}
+
+  login() {
+    sessionStorage.setItem('user', this.userName);
+
+    this.rota.navigate(['home']);
+  }
 
 }
