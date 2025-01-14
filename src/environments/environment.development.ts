e63750428e786f89917dca372a9683b66161dc8c
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2025-01-13 23:10:42 -0400

    Config ambiente de teste e ambiente d eprodução

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyBZBM4AurlXIbEVzhqLNAYqJWCCakdv9NE",
+        authDomain: "control-users-angular.firebaseapp.com",
+        projectId: "control-users-angular",
+        storageBucket: "control-users-angular.firebasestorage.app",
+        messagingSenderId: "379964426925",
+        appId: "1:379964426925:web:f6bd1acf39f7af9fae5424"
+      }
+};
