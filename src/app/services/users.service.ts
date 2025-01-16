b8ceff5e7b12bca8e864511596bf52852011d265
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2025-01-15 21:38:13 -0400

    create var databasestore

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFireStore } from '@angular/fire/compat/firestore'
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private dataBaseStore: AngularFireStore) { }
 }
