b99ed726fe35392507e6b44dd8be04d806b75d35
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2025-01-15 22:20:43 -0400

    add parametro para user

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,26 +2,28 @@ import { Injectable } from '@angular/core';
 
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  
 
   constructor(private dataBaseStore: AngularFirestore) { }
 
   getAllUsers(){
     return this.dataBaseStore.collection('users', user => user.orderBy('name')).valueChanges({idField: 'firebaseId'}) as Observable<any[]>;
   }
-  addUser(user: any){
+  addUser(user: User){
     return this.dataBaseStore.collection('users').add(user);
   }
   
-  update(userId: string, user: any){
+  update(userId: string, user: User){
     return this.dataBaseStore.collection('users').doc(userId).update(user);
   }
 
-  deleteUser(userId: any){
+  deleteUser(userId: User){
     return this.dataBaseStore.collection('users').doc(userId).delete();
   }
 
